a67423cfb0ef497a5b708f81123511298d6f3656
luckychacha <358140284@qq.com>
2021-10-17 16:43:31 +0800

    eidt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
+use rust_decimal::Decimal;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::error::Error;
-use rust_decimal::Decimal;
-use serde::{Serialize, Deserialize};
-
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Token {
@@ -13,22 +12,77 @@ pub struct Token {
 
 pub type Tokens = Vec<Token>;
 
-pub struct Config {
-    pub ids: String,
-    pub vs_currencies: String,
+pub enum Configs {
+    Price { ids: String, vs_currencies: String },
+    Id { symbols: String },
+}
+
+pub trait Query {
+    fn query(&self) -> Result<(), Box<dyn Error>>;
+}
+
+impl Query for Configs {
+    fn query(&self) -> Result<(), Box<dyn Error>> {
+        match self {
+            Configs::Price { ids, vs_currencies } => {
+                let url = format!(
+                    "https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies={}&include_24hr_change=true",
+                    ids,
+                    vs_currencies
+                );
+                let res = reqwest::blocking::get(url)?.json::<SimplePrices>()?;
+                // println!("res: {:?}", res);
+                for prices in res {
+                    println!("token id: {}", prices.0);
+                    let price_info = prices
+                        .1
+                        .iter()
+                        .map(|(key, value)| {
+                            if key.ends_with("24h_change") {
+                                return format!("{}:{}\r\n", key, value);
+                            }
+                            format!("vs_currency:{},price:{}\r\n", key, value)
+                        })
+                        .collect::<String>();
+                    println!("{}", price_info);
+                }
+                Ok(())
+            }
+            Configs::Id { symbols } => {
+                let url = "https://api.coingecko.com/api/v3/coins/list";
+                let res = reqwest::blocking::get(url)?.json::<Tokens>()?;
+                let symbols: Vec<&str> = symbols.split(',').collect();
+                let result: Vec<&Token> = res
+                    .iter()
+                    .filter(|&token| symbols.contains(&token.symbol.as_str()))
+                    .collect();
+
+                let mut ids = Vec::<&str>::new();
+                for token in result {
+                    println!(
+                        "token id: [{}], symbol: [{}], name: [{}[",
+                        token.id, token.symbol, token.name
+                    );
+                    ids.push(token.id.as_str());
+                }
+                println!("ids are: {}", ids.join(","));
+                Ok(())
+            }
+        }
+    }
 }
 
 pub type SimplePrices = HashMap<String, SimplePrice>;
 pub type SimplePrice = HashMap<String, Decimal>;
 
-impl Config {
-    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+impl Configs {
+    pub fn init(mut args: std::env::Args) -> Result<Configs, &'static str> {
         let parameter_nums = args.len();
 
         if parameter_nums < 3 {
             return Err("Need at least 2 parameters.");
         }
-        
+
         args.next();
 
         let operate_type = match args.next() {
@@ -37,11 +91,13 @@ impl Config {
         };
 
         if parameter_nums == 3 && "id" != operate_type {
-            return Err("Need 2 parameters to search ids, first is id, second is symbols[comma-seperated].")
+            return Err(
+                "Need 2 parameters to search ids, first is id, second is symbols[comma-seperated].",
+            );
         }
 
         if parameter_nums == 4 && "price" != operate_type {
-            return Err("Need 3 parameters to query price, first is price, second is ids[comma-seperated], third is vs_currencies[comma-seperated].")
+            return Err("Need 3 parameters to query price, first is price, second is ids[comma-seperated], third is vs_currencies[comma-seperated].");
         }
 
         let ids = match args.next() {
@@ -50,74 +106,18 @@ impl Config {
         };
 
         if parameter_nums == 3 {
-            return Ok(Config {
-                ids,
-                vs_currencies: String::from("search-token"),
-            });
+            return Ok(Configs::Id { symbols: ids });
         }
-        
+
         if parameter_nums == 4 {
             let vs_currencies = match args.next() {
                 Some(arg) => arg,
                 None => return Err("Didn't get vs_currencies"),
             };
 
-            return Ok(Config {
-                ids,
-                vs_currencies,
-            });
+            return Ok(Configs::Price { ids, vs_currencies });
         }
 
         Err("Parameter error")
     }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-
-    let url = format!(
-        "https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies={}&include_24hr_change=true",
-        config.ids,
-        config.vs_currencies
-    );
-    let res = reqwest::blocking::get(url)?
-            .json::<SimplePrices>()?;
-    // println!("res: {:?}", res);
-    for prices in res {
-        println!("token id: {}", prices.0);
-        let price_info = prices.1.iter().map(|(key, value)| {
-            if key.ends_with("24h_change") {
-                return format!(
-                    "{}:{}\r\n",
-                    key,
-                    value
-                );
-            }
-            format!(
-                "vs_currency:{},price:{}\r\n",
-                key,
-                value
-            )
-        }).collect::<String>();
-        println!("{}", price_info);
-    }
-    Ok(())
-}
-
-pub fn search_by_symbol(config: Config) -> Result<(), Box<dyn Error>> {
-    let url = "https://api.coingecko.com/api/v3/coins/list";
-    let res = reqwest::blocking::get(url)?
-            .json::<Tokens>()?;
-    let symbols: Vec<&str> = config.ids.split(",").collect();
-    let result: Vec<&Token> = res.iter().filter(|&token| {
-        symbols.contains(&token.symbol.as_str())
-    }).collect();
-    
-    let mut ids = Vec::<&str>::new();
-    for token in result {
-        println!("token id: [{}], symbol: [{}], name: [{}[", token.id, token.symbol, token.name);
-        ids.push(token.id.as_str());
-    }
-    println!("ids are: {}", ids.join(","));
-    Ok(())
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use std::{process};
-use cgtool::{Config, run, search_by_symbol};
+use cgtool::{Configs, Query};
+use std::process;
 
 fn main() {
-    let config: Config = Config::new(std::env::args()).unwrap_or_else(|message| {
+    let config: Configs = Configs::init(std::env::args()).unwrap_or_else(|message| {
         eprintln!("Problem parsing arguments: {}", message);
         process::exit(1);
     });
-    if config.vs_currencies == "search-token" {
-        if let Err(e) = search_by_symbol(config) {
-            eprintln!("Application Error: {}", e);
-            process::exit(1);
-        }
-    } else {
-        if let Err(e) = run(config) {
-            eprintln!("Application Error: {}", e);
-            process::exit(1);
-        }
+    if let Err(e) = config.query() {
+        eprintln!("Application Error: {}", e);
+        process::exit(1);
     }
+    // if config.vs_currencies == "search-token" {
+    //     if let Err(e) = search_by_symbol(config) {
+    //         eprintln!("Application Error: {}", e);
+    //         process::exit(1);
+    //     }
+    // } else {
+    //     if let Err(e) = run(config) {
+    //         eprintln!("Application Error: {}", e);
+    //         process::exit(1);
+    //     }
+    // }
 }
